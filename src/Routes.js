e33d195ad437c9a302d9ffec9222fe44a1047fbd
Matheus <giehlmatheus96@gmail.com>
2022-12-31 11:34:19 -0300

    Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Routes , Route } from "react-router-dom";
 
 import Home from './pages/Home';
 import Mapas from './pages/Mapas';
+import Agentes from './pages/Agentes';
 
 export default () => {
     return(
         <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/mapas" element={<Mapas />} />
+            <Route exact path="/agentes" element={<Agentes />} />
         </Routes>
     ) 
 };
