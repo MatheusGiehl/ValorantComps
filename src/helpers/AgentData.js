cf938b451820319c182f85de8cf95f46bc9e898f
Matheus <giehlmatheus96@gmail.com>
2023-01-01 21:35:02 -0300

    AgentData

---
+++ src/helpers/AgentData.js
@@ -0,0 +1,27 @@
+function AgentData() {
+
+const agents = [
+    {agentName:'Sova', imgUrl:''},
+    {agentName:'Fade', imgUrl:''},
+    {agentName:'Breach', imgUrl:''},
+    {agentName:'Skye', imgUrl:''},
+    {agentName:'KAY/O', imgUrl:''},
+    {agentName:'Jett', imgUrl:''},
+    {agentName:'Raze', imgUrl:''},
+    {agentName:'Neon', imgUrl:''},
+    {agentName:'Phoenix', imgUrl:''},
+    {agentName:'Reyna', imgUrl:''},
+    {agentName:'Yoru', imgUrl:''},
+    {agentName:'Omen', imgUrl:''},
+    {agentName:'Viper', imgUrl:''},
+    {agentName:'Brimstone', imgUrl:''},
+    {agentName:'Astra', imgUrl:''},
+    {agentName:'Harbor', imgUrl:''},
+    {agentName:'Killjoy', imgUrl:''},
+    {agentName:'Cypher', imgUrl:''},
+    {agentName:'Chamber', imgUrl:''},
+    {agentName:'Sage', imgUrl:''},
+] 
+}
+
+export default AgentData
