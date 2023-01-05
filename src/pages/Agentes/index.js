ab46b1ae4570fc568dac4573b92071176b1e0552
Matheus <giehlmatheus96@gmail.com>
2023-01-04 22:24:52 -0300

    init agent page

---
+++ src/pages/Agentes/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HeaderAgents from "../../components/HeaderAgents";
+
+function Agentes() {
+    return (
+        <HeaderAgents/>
+    )
+}
+
+export default Agentes
